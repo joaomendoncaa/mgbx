5a7d58a34b20eb85181e3ac48c4eaf75a7808cfc
joaomendoncaa <joaommm0312@gmail.com>
2021-06-05 19:27:40 +0100

    Toast fix

--- src/Toast.js
+++ src/Toast.js
@@ -8,7 +8,7 @@ class Toast {
     return this._toast
   }
 
-  displayMessage(messageString, messageDuration = 3000) {
+  displayMessage(messageString, messageDuration) {
     this.toast.textContent = messageString
     this.toast.style.display = 'flex'
     setTimeout(() => {
@@ -27,7 +27,7 @@ class Toast {
     //get body to use it as a parent for the toast html span
     const body = document.querySelector('body')
     //set the default style
-    this.toast.setStyle({
+    this.setStyle({
       display: 'none',
       color: '#ffffff',
       position: 'absolute',
